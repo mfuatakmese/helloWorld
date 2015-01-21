1fe9825b05e65557c5ba10e26c3ca4d04f664f58
mFuatAkmese <mFuatAkmese@hotmail.com>
2015-01-21 20:11:45 +0200

    hello

---
+++ HelloWorld.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of HelloWorld
+ *
+ * @author 3M
+ */
+class HelloWorld {
+    //put your code here
+    private $hi;
+    
+    public function setHi($hi) {
+        $this->hi = $hi;
+    }
+    
+    public function getHi() {
+        return $this->hi;
+    }
+}
